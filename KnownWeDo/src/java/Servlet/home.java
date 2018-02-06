f03fcd2058c9408bd48fce8268a6a4613923cb2e
samatsak <tar.samatsak@gmail.com>
2018-02-06 19:27:19 +0700

    58130500101

--- KnownWeDo/src/java/Servlet/home.java
+++ KnownWeDo/src/java/Servlet/home.java
@@ -17,7 +17,7 @@ import javax.servlet.http.HttpServletResponse;
  *
  * @author JVVQX
  */
-public class index extends HttpServlet {
+public class home extends HttpServlet {
 
     /**
      * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
