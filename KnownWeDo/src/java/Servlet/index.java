045265c62eaa171b0f15e050f53be1b8d8b9abe6
mnkych <mnkych@gmail.com>
2018-02-07 07:44:15 +0700

    Revert "58130500101"
    This reverts commit f03fcd2058c9408bd48fce8268a6a4613923cb2e.

--- KnownWeDo/src/java/Servlet/index.java
+++ KnownWeDo/src/java/Servlet/index.java
@@ -17,7 +17,7 @@ import javax.servlet.http.HttpServletResponse;
  *
  * @author JVVQX
  */
-public class home extends HttpServlet {
+public class index extends HttpServlet {
 
     /**
      * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
