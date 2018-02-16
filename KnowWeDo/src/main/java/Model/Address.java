d9cedd48a880e5d073150913774632144326ec82
samatsak <tar.samatsak@gmail.com>
2018-02-16 22:35:56 +0700

    Assign Attribute of Address Class

--- KnowWeDo/src/main/java/Model/Address.java
+++ KnowWeDo/src/main/java/Model/Address.java
@@ -10,5 +10,32 @@ package Model;
  * @author JVVQX
  */
 public class Address {
+    private String province;
+    private String amphur;
+
+    public Address() {
+    }
+
+    public Address(String province, String amphur) {
+        this.province = province;
+        this.amphur = amphur;
+    }
+
+    public String getProvince() {
+        return province;
+    }
+
+    public void setProvince(String province) {
+        this.province = province;
+    }
+
+    public String getAmphur() {
+        return amphur;
+    }
+
+    public void setAmphur(String amphur) {
+        this.amphur = amphur;
+    }
+    
     
 }
